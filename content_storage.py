2de94fe16b884f2f6e8bce16c1e5dfa84fb46591
jeadi <jeadi@amazon.com>
2022-05-01 09:42:06 +1000

    add basic storage layer

---
+++ content_storage.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timedelta
+from typing import Dict, List
+import json 
+
+import sqlite3
+from peewee import AutoField, CharField, DateTimeField, ForeignKeyField, Model, SqliteDatabase, TextField
+
+from content_providers import InternetContent
+
+DB= SqliteDatabase("the-internet.db")
+class BaseModel(Model):
+    class Meta:
+        database = DB
+
+class InternetLocation(BaseModel):
+    location_type = CharField(unique=True)
+
+class InternetContent(BaseModel):
+    id = CharField(unique=True)
+    timestamp = DateTimeField()
+    title = CharField()
+    url = CharField()
+
+    location = ForeignKeyField(InternetLocation, backref="contents")
+
+    # JSON encoded
+    content: TextField()
+
+
+class ContentStorage(object):
+
+    def __init__(self) -> None:
+        with DB:
+            DB.create_tables([InternetContent, InternetLocation])
+
+    def _get_or_create_internet_locations(self, content: List[InternetContent]) -> Dict[str, InternetContent]:
+        """ Maps the content types of InternetContentProvider to their InternetLocation. Does not
+        recreate InternetLocation if they exist. 
+        """
+        content_types = list(map(lambda x: x.content_type, content))
+        type_location_map = {}
+        for t in content_types:
+            l, _ = InternetLocation.get_or_create(location_type=t)
+            type_location_map[t] = l
+        return type_location_map
+
+    def save(self, internet_content: List[InternetContent]):
+        """ Saves a list of internet content.
+        
+        """
+        content_type_to_internet_location = self._get_or_create_internet_locations(internet_content)
+
+        # The bulk_create API doesn't set the fk value automatically.
+        with DB.atomic():
+            for c in internet_content:
+                InternetContent.get_or_create(
+                    id=c.id,
+                    defaults = {
+                        "timestamp" : c.timestamp,
+                        "title" : c.title,
+                        "url" : c.url,
+                        "location" : content_type_to_internet_location[c.content_type],
+                        "content" : json.dumps(c.content)    
+                    }
+                )
+
+    def get(self, last_n_days=7) -> List[InternetContent]:
+        return []
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import requests
 import bs4
 
 from content_providers import HackerNewsContentProvider, IndieHackerContentProvider, InternetContent
+from content_storage import ContentStorage
 
 class InvalidRequestException(Exception):
     pass
 
 
 content_providers = [HackerNewsContentProvider(), IndieHackerContentProvider()]
-internet: Dict[str, List[InternetContent]] = {}
+storage = ContentStorage()
 
 for c in content_providers:
     resp = requests.get(c.getBaseWebsite())
@@ -18,6 +19,4 @@ for c in content_providers:
         raise InvalidRequestException("Invalid status code {} from website {}. {}".format(resp.url, c.getBaseWebsite, resp.reason))
 
     soup = bs4.BeautifulSoup(resp.content, "html.parser")
-    internet[c.getContentId()] = c.getContent(soup)
-
-print(internet)
+    storage.save(c.getContent(soup))
